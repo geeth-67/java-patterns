bb17ac6b952def0be49001e8426744c3a63f6958
vishwageethanjana <geeth.me67@gmail.com>
2026-05-06 10:05:10 +0530

    Design patterns JAVA 06.05.2026

---
+++ src/FactoryPattern/Package.java
@@ -0,0 +1,15 @@
+package FactoryPattern;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Package {
+
+    protected List<Decoration> deco = new ArrayList<>();
+
+    public Package() {
+        createPackage();
+    }
+
+    protected abstract void createPackage();
+}
