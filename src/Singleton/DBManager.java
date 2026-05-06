432e7774aa032ee54bd11618d6912bcf41f9d3dc
vishwageethanjana <geeth.me67@gmail.com>
2026-05-06 13:35:07 +0530

    Design patterns JAVA 06.05.2026

--- src/Singleton/DBManager.java
+++ src/Singleton/DBManager.java
@@ -2,11 +2,20 @@ package Singleton;
 
 public class DBManager {
 
-    private static DBManager dbManager = new DBManager();
+    private static volatile DBManager dbManager;  // eager loading
 
     private DBManager() {}     // Private contractor
 
     public static DBManager getDbManager() {
+
+        if (dbManager == null) {
+
+            synchronized (DBManager.class) {
+                if (dbManager == null) {     // Double-checking singleton
+                    dbManager = new DBManager();
+                }
+            }
+        }
         return dbManager;
     }
 }
