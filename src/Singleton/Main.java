703f3d287218b2d2814c5f4d34c073f6f59b1528
vishwageethanjana <geeth.me67@gmail.com>
2026-05-06 13:35:09 +0530

    Design patterns JAVA 06.05.2026

--- src/Singleton/Main.java
+++ src/Singleton/Main.java
@@ -4,10 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-        DBManager dbManager = DBManager.getDbManager();   // called class method , not a object
-        DBManager dbManager1 = DBManager.getDbManager();
+        DBManager dbManager = DBManager.getDbManager();   // called class method , not an object
+        DBManager dbManager01 = DBManager.getDbManager();
 
-        System.out.println(dbManager);   //Singleton.DBManager@8efb846      same hashcode
-        System.out.println(dbManager1);  //Singleton.DBManager@8efb846
+        System.out.println(dbManager);   // Singleton.DBManager@8efb846      same hashcode
+        System.out.println(dbManager01);  // Singleton.DBManager@8efb846
     }
 }
